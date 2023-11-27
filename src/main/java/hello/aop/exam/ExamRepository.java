a3d697e9a88ff01942f6d4527697d687b8ffeea0
dokdogalmaegi <dokdogalmaegi@gmail.com>
2023-11-27 12:01:53 +0900

    excpetion

---
+++ src/main/java/hello/aop/exam/ExamRepository.java
@@ -0,0 +1,22 @@
+package hello.aop.exam;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExamRepository {
+
+    private static int seq = 0;
+
+    /**
+     * 5 번에 한 번씩 예외 발생
+     * @param itemId
+     * @return
+     */
+    public String save(String itemId) {
+        seq++;
+        if (seq % 5 == 0) {
+            throw new IllegalStateException("예외 발생");
+        }
+        return "ok";
+    }
+}
